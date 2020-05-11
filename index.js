1fc4eaa783c94e29d730d2becf2f4ace074f0b88
wuchau <wuchau@connect.ust.hk>
2020-05-11 13:57:35 +0800

    test

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express'),
 	http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
-
 const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req, res, next) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	next();
+});
+app.get('/dishes', (req, res, next) => {
+	res.end('Will send all the dishes to you!!');
+});
+app.post('/dishes', (req, res, next) => {
+	res.end(
+		'will add the dish: ' +
+			req.body.name +
+			'with details: ' +
+			req.body.description
+	);
+});
+//hi
+app.put('/dishes', (req, res, next) => {
+	res.statusCode = 403;
+	res.end('Put operation not supported on /dishes');
+});
+
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
 	console.log(req.headers);
